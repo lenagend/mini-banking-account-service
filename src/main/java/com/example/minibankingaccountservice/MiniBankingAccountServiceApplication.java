afe1dec59c90bc8ae2a38d0870a9f1eeb810f8e9
lenagend <lenagend@naver.com>
2024-03-20 16:33:32 +0900

    Initial commit

---
+++ src/main/java/com/example/minibankingaccountservice/MiniBankingAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.minibankingaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBankingAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniBankingAccountServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minibankingaccountservice/MiniBankingAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minibankingaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBankingAccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
