7755774f9a1424097dcaff064bc7059140d567c3
lenagend <lenagend@naver.com>
2024-03-21 17:30:11 +0900

    feat(AccountRepository, application.properties): AccountRepository, application.properties에 기능, 설정 추가
    AccountRepository: userId로 계좌를 찾는 메서드 추가.
    application.properties 서버 포트번호를 8082로 정의, userService의 url을 정의
    
    BREAKING CHANGE: 새 파일. 기존 코드에 영향을 주지 않습니다.

--- src/main/java/repository/AccountRepository.java
+++ src/main/java/repository/AccountRepository.java
@@ -3,5 +3,8 @@ package repository;
 import entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findByUserId(Long userId);
 }
