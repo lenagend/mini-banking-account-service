ef65932a34bced4bdad5a7e0a79f09964b46e588
lenagend <lenagend@naver.com>
2024-03-21 14:03:48 +0900

    feat(Account 및 AccountRepository): Account 및 AccountRepository 추가
    이 커밋은 계좌 데이터를 저장하고 검색하기 위한 Account 클래스와 AccountRepository 인터페이스를 도입합니다.
    Account 클래스는 id, userId, balanace 같은 계좌 데이터의 구조를 정의합니다.
    AccountRepository는 save, findById, delete와 같은 일반적인 데이터 작업을 위한 추상 메서드를 제공합니다.
    
    BREAKING CHANGE: 없음. 이것은 새로운 기능이며 기존 코드에 영향을 주지 않습니다.
    
    Closes X

---
+++ src/main/java/entity/Account.java
@@ -0,0 +1,40 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Long userId;
+    private Double balance;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+}
